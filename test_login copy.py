752b6d5fd46c82cb7eb08b7ce8c090ce93f235c5
sercutos <sergio.cubero@gmail.com>
2024-06-12 20:56:17 +0200

    cambio a decouple

--- test_login copy.py
+++ test_login copy.py
@@ -1,12 +1,7 @@
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
-#from decouple import config
+from decouple import config
 import os
-from dotenv import load_dotenv, dotenv_values
-
-load_dotenv()
-
-
 
 
 def test_run(playwright: Playwright) -> None:
@@ -16,9 +11,9 @@ def test_run(playwright: Playwright) -> None:
     page.goto("https://test-openecoe.umh.es/")
     page.goto("https://test-openecoe.umh.es/login?returnUrl=%2Fecoe")
     page.get_by_placeholder("Correo electrónico").click()    
-    page.get_by_placeholder("Correo electrónico").fill(os.getenv('EMAIL'))
+    page.get_by_placeholder("Correo electrónico").fill(config('EMAIL'))
     page.get_by_placeholder("Contraseña").click()
-    page.get_by_placeholder("Contraseña").fill(os.getenv('PASSWORD'))
+    page.get_by_placeholder("Contraseña").fill(config('PASSWORD'))
     page.get_by_role("button", name="Iniciar sesión").click()
     # ---------------------
     context.close()
