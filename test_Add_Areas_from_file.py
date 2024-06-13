fb61c832826c286f8ab8abb1364e4f0c007b52ab
sercutos <sergio.cubero@gmail.com>
2024-06-13 19:25:40 +0200

    cambios 13/06/24 19:25

--- test_Add_Areas_from_file.py
+++ test_Add_Areas_from_file.py
@@ -1,31 +1,28 @@
 from playwright.sync_api import Page
 import pytest
 import random
+from decouple import config
 
 #EMAIL = 'sergio.cubero@uv.es'
 #PASSWORD = 'ecoe20jornada'
 
-EMAIL = 'ecoe@umh.es'
-PASSWORD = 'Kui0chee'
-ECOE_NAME = 'ECOE_NUEVA_26'
-SERVER = 'http://sauron.uv.es:8081'
 
 def test_create_ECOE(page: Page):
     # LOGIN
-    page.goto(SERVER)
+    page.goto(config('SERVER'))
     page.get_by_placeholder("Correo electrónico").click()    
-    page.get_by_placeholder("Correo electrónico").fill(EMAIL)
+    page.get_by_placeholder("Correo electrónico").fill(config('EMAIL'))
     page.get_by_placeholder("Contraseña").click()
-    page.get_by_placeholder("Contraseña").fill(PASSWORD)
+    page.get_by_placeholder("Contraseña").fill(config('PASSWORD'))
     page.get_by_role("button", name="Iniciar sesión").click()
     # Add AREAS
-    page.locator("nz-card").filter(has_text=ECOE_NAME).get_by_role("link").click()
+    page.locator("nz-card").filter(has_text=config('ECOE_NAME')).get_by_role("link").click()
     page.get_by_role("link", name="Configurar").click()
     page.get_by_role("button", name="Importar").click()
     
     with page.expect_file_chooser() as fc_info:        
         page.get_by_role("button", name="Pinche aquí o arrastre un").click()
     file_chooser = fc_info.value
-    file_chooser.set_files("Areas.csv")
+    file_chooser.set_files(config('AREAS_CVS'))
     
     
--- test_create_ECOE.py
+++ test_create_ECOE.py
@@ -1,22 +1,21 @@
 from playwright.sync_api import Page
 import pytest
 import random
+from decouple import config
 
 #EMAIL = 'sergio.cubero@uv.es'
 #PASSWORD = 'ecoe20jornada'
 
-EMAIL = 'ecoe@umh.es'
-PASSWORD = 'Kui0chee'
 ECOE_NAME = f"ECOE_NUEVA_{random.randint(0,99)}"
-SERVER = 'http://sauron.uv.es:8081'
+
 
 def test_create_ECOE(page: Page):
     # LOGIN
-    page.goto(SERVER)
-    page.get_by_placeholder("Correo electrónico").click()    
-    page.get_by_placeholder("Correo electrónico").fill(EMAIL)
-    page.get_by_placeholder("Contraseña").click()
-    page.get_by_placeholder("Contraseña").fill(PASSWORD)
+    page.goto(config('SERVER'))
+    page.get_by_placeholder("Correo electrónico").click()        
+    page.get_by_placeholder("Correo electrónico").fill(config('EMAIL'))
+    page.get_by_placeholder("Contraseña").click()    
+    page.get_by_placeholder("Contraseña").fill(config('PASSWORD'))
     page.get_by_role("button", name="Iniciar sesión").click()
     # Create ECOE
     page.goto("http://sauron.uv.es:8081/ecoe")
